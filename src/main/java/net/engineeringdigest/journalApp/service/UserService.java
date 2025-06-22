48e7ec06b9529834692a483d86765ee6e0cbb1d3
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-06-22 13:49:13 +0530

    Add Authentication to "/journal" endpoint

--- src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -21,7 +21,7 @@ public class UserService {
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveEntry(User user){
+    public void saveNewUser(User user){
         try {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setRoles(Arrays.asList("USER"));
@@ -31,7 +31,7 @@ public class UserService {
         }
     }
 
-    public void saveNewUser(User user){
+    public void saveUser(User user){
         userRepository.save(user);
     }
 
@@ -50,4 +50,6 @@ public class UserService {
     public User findByUserName(String userName){
         return userRepository.findByUserName(userName);
     }
+
+
 }
